8c98281077fc28e94b2ee3daceb9d30afe4b15c5
Vikylli <romasviktoria4@gmail.com>
2025-04-10 09:06:15 +0500

    fix 5 practika

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,86 +1,111 @@
 @extends('layouts.layout')
 
 @section('content')
-
 <div class="content-wrapper">
-   <!-- Content Header (Page header) -->
-   <section class="content-header">
-     <div class="container-fluid">
-       <div class="row mb-2">
-         <div class="col-sm-6">
-           <h1>Категории</h1>
-         </div>
-         <div class="col-sm-6">
-           <ol class="breadcrumb float-sm-right">
-             <li class="breadcrumb-item"><a href="#">Home</a></li>
-             <li class="breadcrumb-item active">Главная</li>
-           </ol>
-         </div>
-       </div>
-     </div><!-- /.container-fluid -->
-   </section>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Simple Tables</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Simple Tables</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
 
-   <!-- Main content -->
-   <section class="content">
-     <div class="container-fluid">
-       <div class="row ">
-         <div class="col-12">
-           <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Список категорий</h3>
-            </div>
-                <div class="card-body">
-                <a href=" {{ route ('categories.create')}}" class="btn btn-primary mb-3">Добавить категорию</а>
-            @if (count ($categories) )
-            <div class="table-responsive">
-                <table class="table table-bordered table-hover text-nowrap">
-            <thead>
-                <tr>
-                    <th style="width: 30px">#</th>
-                    <th>Наименование</th>
-                    <th>Slugs</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-        <tbody>
-            @foreach ( $categories as $category )
-                <tr>
-                    <td>{{ $category->id }}</td>
-                    <td>{{ $category->title }}</td>
-                    <td>{{ $category->slug }}</td>
-                    <td>
-                    <a href="{{route ('categories.edit', ['category'=> $category->id ]) }}"  class= "btn btn-info btn-sm float-left mr-1">
-                        <i class="fas fa-pencil-alt"></i>
-                    </a>
-                    <form action="{{ route ('categories.destroy', ['category' => $category->id])}}" method= "post" class="float-left" >
-                        @csrf
-                         @method ( 'DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm"
-                                onclick= "return confirm('Подтвердите удаление')">
-
-                                <i class="fas fa-trash-alt"></i>
-                        </button>
-                    </form> 
-                    </td>
-                </tr>
-                @endforeach
-                </tbody>
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Bordered Table</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>Task</th>
+                      <th>Progress</th>
+                      <th style="width: 40px">Label</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1.</td>
+                      <td>Update software</td>
+                      <td>
+                        <div class="progress progress-xs">
+                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-danger">55%</span></td>
+                    </tr>
+                    <tr>
+                      <td>2.</td>
+                      <td>Clean database</td>
+                      <td>
+                        <div class="progress progress-xs">
+                          <div class="progress-bar bg-warning" style="width: 70%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-warning">70%</span></td>
+                    </tr>
+                    <tr>
+                      <td>3.</td>
+                      <td>Cron job running</td>
+                      <td>
+                        <div class="progress progress-xs progress-striped active">
+                          <div class="progress-bar bg-primary" style="width: 30%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-primary">30%</span></td>
+                    </tr>
+                    <tr>
+                      <td>4.</td>
+                      <td>Fix and squish bugs</td>
+                      <td>
+                        <div class="progress progress-xs progress-striped active">
+                          <div class="progress-bar bg-success" style="width: 90%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-success">90%</span></td>
+                    </tr>
+                  </tbody>
                 </table>
-                </div>
-                @else
-                <p>Категорий пока нет...</p>
-                @endif
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                <ul class="pagination pagination-sm m-0 float-right">
+                  <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                </ul>
+              </div>
             </div>
+            <!-- /.card -->
 
-       <!-- /.card-body -->
-                <div class="card-footer clearfix"></div>
-           </div>
 
-         </div>
-        
-       </div>
-     </div><!-- /.container-fluid -->
-   </section>
-</div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
 
 @endsection
